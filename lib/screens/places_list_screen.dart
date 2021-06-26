91498d7315b1f6f84cf22834d04082772411ec93
Russkazkin <ruslan@skazkin.su>
2021-06-26 04:30:35 +0300

    Add provider package

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class PlacesListScreen extends StatelessWidget {
-  const PlacesListScreen({Key? key}) : super(key: key);
+  const PlacesListScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
