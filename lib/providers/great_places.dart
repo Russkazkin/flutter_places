2deac40984f2e7f9fd4b3e83fe56ff4b7fb31ef2
Russkazkin <ruslan@skazkin.su>
2021-07-15 07:00:09 +0300

    Install uuid package

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
+import 'package:uuid/uuid.dart';
+
 import '../models/place.dart';
 
 class GreatPlaces with ChangeNotifier {
@@ -6,5 +10,10 @@ class GreatPlaces with ChangeNotifier {
 
   List<Place> get items {
     return [..._items];
+  }
+
+  void addPlace(String title, File image) {
+    var uuid = Uuid();
+    final newPlace = Place(id: uuid.v1(), title: Uuid, location: location, image: image)
   }
 }
